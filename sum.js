a8c892145ef9ebe5797bceaa3ab50e5f59e1256b
brittanyshelley <brittany.j.shelley@gmail.com>
2023-10-21 22:06:45 +0000

    initial commit

---
+++ sum.js
@@ -0,0 +1,11 @@
+const args = process.argv.slice(2);
+console.log(args);
+
+const num1 = parseInt(args[0], 10);
+const num2 = parseInt(args[1], 10);
+const sum = num1 + num2;
+console.log(sum);
+
+
+
+
