bd187740a8885c7bb9a99f4b3fd5f32d6d627cdf
brittanyshelley <brittany.j.shelley@gmail.com>
2023-10-24 18:15:00 -0600

    function done

--- functions/callbacks-intro/findWaldo.js
+++ functions/callbacks-intro/findWaldo.js
@@ -3,13 +3,14 @@ const findWaldo = function(names, found) {
   for (let i = 0; i < names.length; i++) {
     let name = names[i];
     if (name === "Waldo") {
-      found();   // execute callback
+      let index = i;
+      found(index);   // execute callback
     }
   }
 }
 
-const actionWhenFound = function() {
-  console.log("Found him!");
+const actionWhenFound = function(index) {
+  console.log(`Found Waldo at index: ${index}`);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
